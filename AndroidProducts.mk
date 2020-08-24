ca639cbde496a5c7839d697db2a6e450e4999b99
Josh Chasky <jchasky@gmail.com>
2020-08-24 17:27:14 -0400

    DU-ify
    Change-Id: I75985e589b640dad48cf7efec5a9dcf3ce6737fe

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sunfish.mk \
+    $(LOCAL_DIR)/aosp_sunfish.mk
 
-COMMON_LUNCH_CHOICES := \
-    aosp_sunfish-userdebug \
+#COMMON_LUNCH_CHOICES := \
+#    aosp_sunfish-userdebug
