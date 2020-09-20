a456841a9d5069d5ba38ac82b0ae06d6ce4ae4b1
Josh Chasky <jchasky@gmail.com>
2020-09-20 18:43:50 -0400

    Expand AOSP compiled packages list for vendor
    * It's always better when we can get what we need from source instead of including a prebuilt

--- device.mk
+++ device.mk
@@ -375,7 +375,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.sm6150 \
     hardware.google.light@1.0.vendor \
-    hardware.google.light@1.1-service
+    hardware.google.light@1.1-service \
+    hardware.google.light@1.1.vendor:64
 
 # Memtrack HAL
 PRODUCT_PACKAGES += \
@@ -947,11 +948,21 @@ PRODUCT_PACKAGES += \
 
 # Build necessary packages for vendor
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor:32 \
+    chre \
     ese_spi_st \
+    libavservices_minijail_vendor:32 \
+    libbatching \
     libcodec2_hidl@1.0.vendor:32 \
     libcodec2_vndk.vendor \
     libdrm.vendor \
+    libgooglecamerahal.vendor:64 \
+    libgooglecamerahalutils.vendor:64 \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
     libjson \
+    libkeymaster_messages.vendor:64 \
+    libkeymaster_portable.vendor:64 \
     libmedia_ecoservice.vendor \
     libnetfilter_conntrack:64 \
     libnfnetlink:64 \
@@ -961,13 +972,19 @@ PRODUCT_PACKAGES += \
     libnos_datagram_citadel:64 \
     libnosprotos:64 \
     libnos_transport:64 \
+    lib_profiler:64 \
+    libpuresoftkeymasterdevice.vendor:64 \
     libsensorndkbridge:64 \
+    libsoft_attestation_cert.vendor:64 \
     libtinycompress \
     libtinyxml \
+    libwifi-hal:64 \
     libwifi-hal-qcom \
     nos_app_avb:64 \
+    nos_app_identity:64 \
     nos_app_keymaster:64 \
     nos_app_weaver:64 \
+    sound_trigger.primary.sm6150 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@1.1.vendor \
     vendor.display.config@1.2.vendor \
